828a65a48474733db6109c9c8e74825d68bff68a
John Delizo <johndelizo99@gmail.com>
2021-01-31 21:59:39 -0800

    fixed typo in server.js

--- server.js
+++ server.js
@@ -2,4 +2,12 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`https://localhost:${PORT}`))
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// const exbhs = require("express-handlebars");
+// app.engine("handlebars", exhbs({ defaultLayout: "main"}));
+// app.set("view engine", "handlebars")
+
+app.listen(PORT, () => console.log(`http://localhost:${PORT}`))
