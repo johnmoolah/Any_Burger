6a8940a299d714c82ece60094a83ce09bf11258f
John Delizo <johndelizo99@gmail.com>
2021-02-04 05:22:33 -0800

    created GET request to return index file

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const exhbs = require("express-handlebars");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
@@ -6,8 +7,9 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// const exbhs = require("express-handlebars");
-// app.engine("handlebars", exhbs({ defaultLayout: "main"}));
-// app.set("view engine", "handlebars")
+const exbhs = require("express-handlebars");
+
+app.engine("handlebars", exhbs({ defaultLayout: "main"}));
+app.set("view engine", "handlebars")
 
 app.listen(PORT, () => console.log(`http://localhost:${PORT}`))
