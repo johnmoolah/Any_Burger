43545b1e24b4c80553c4c6a9ae900a2f5ee73b33
John Delizo <johndelizo99@gmail.com>
2021-02-08 04:19:30 -0800

    imported api-routes file into server

--- server.js
+++ server.js
@@ -2,19 +2,16 @@ const express = require("express");
 const exhbs = require("express-handlebars");
 const app = express();
 const PORT = process.env.PORT || 5000;
-
+const apiroutes = require("./routes/api-routes")
 
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use("/", apiroutes);
+
 
 const exbhs = require("express-handlebars");
-
 app.engine("handlebars", exhbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars")
-
-app.get("/", (req, res) => {
-    res.render("index");
-});
 
 app.listen(PORT, () => console.log(`http://localhost:${PORT}`))
