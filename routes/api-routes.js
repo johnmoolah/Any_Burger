c72c5f641a540c4ceb952a39b600b6f3f610724c
John Delizo <johndelizo99@gmail.com>
2021-02-10 03:14:58 -0800

    fixed mysql connection

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,7 +1,15 @@
 const express = require("express");
+const connection = require("../config/connection");
 const router = express.Router();
 
 router.get("/", (req, res) => {
+    connection.query("SELECT * FROM burgers", (err, data) => {
+        console.log(result)
+        if (err) {
+            console.log(err);
+        } else
+        console.log("queried", data)
+    });
     res.render("index", { burger_data: ["burger", "more"]});
 });
 
