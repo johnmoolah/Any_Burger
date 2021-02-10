421b882409542c731e30b21efe1417179f7e5e6f
John Delizo <johndelizo99@gmail.com>
2021-02-10 03:17:21 -0800

    fixed mysql connection

--- config/connection.js
+++ config/connection.js
@@ -3,14 +3,12 @@ const mysql = require('mysql');
 const connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
+    user: "root",
     password: 'Lexo510$',
     database: 'burgers_db'
 });
 
 connection.connect((err) => {
-    if (err) {
-        console.log(err);
-        return;
-    } 
+    if (err) throw err;
         console.log("connected: ", connection.threadId);
     });
